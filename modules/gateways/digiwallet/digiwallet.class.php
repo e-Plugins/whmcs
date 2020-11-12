9a043002475fa1525f31d23a76abcfdabd74a903
Celia <celia@targetmedia.eu>
2020-11-12 11:33:57 +0100

    Minor changes & removed old files

--- modules/gateways/digiwallet/digiwallet.class.php
+++ modules/gateways/digiwallet/digiwallet.class.php
@@ -16,7 +16,7 @@ class DigiWalletCore
 {
     // Constants
 
-    const APP_ID = 'dw_whmcs7.9.2_1.1';
+    const APP_ID = 'dw_whmcs7.9.2_1.2';
 
     const MIN_AMOUNT            = 84;
 
--- modules/gateways/digiwallet/digiwalletPayment.php
+++ modules/gateways/digiwallet/digiwalletPayment.php
@@ -477,9 +477,6 @@ class digiwalletPayment
         if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
             //ip from share internet
             $ip = $_SERVER['HTTP_CLIENT_IP'];
-        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            //ip pass from proxy
-            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
         } else {
             $ip = $_SERVER['REMOTE_ADDR'];
         }
